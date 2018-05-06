3bc7dbbac671dd0e912b2be44036a65f3c9615ef
DarchnessX <DarchnessX@hotmail.com>
2018-05-06 05:46:21 -0400

    comments
    oaufnbiagh jr

--- Driver.java
+++ Driver.java
@@ -5,6 +5,7 @@ import java.lang.Math;
 
 public class Driver
 {
+	// Private vars
 	private int enHr;
 	private int enMin;
 	private int exHr;
@@ -14,10 +15,10 @@ public class Driver
 	private int fee;
 	private int tempFee;
 	private boolean isEarly;
-
+	// Default constructor 
 	public Driver()
 	{}
-
+	// Constructor
 	public Driver(int entryHour, int entryMinute, int exitHour, int exitMinute, boolean member)
 	{
 		enHr = entryHour;
@@ -26,17 +27,17 @@ public class Driver
 		exMin = exitMinute;
 		mem = member;
 	}
-
+	// Calculate duration of stay
 	public void calcDuration(int enHr, int enMin, int exHr, int exMin)
 	{
 		duration = ((exHr - enHr) * 60) + (exMin - enMin);
 	}
-
+	// Get method for duration
 	public int getDuration()
 	{
 		return duration;
 	}
-
+	// Check to see if driver qualifies for early rates
 	public void checkEarly(int enHr, int enMin, int exHr, int exMin)
 	{
 		if (enHr < 10)
@@ -51,24 +52,31 @@ public class Driver
 			isEarly = false;
 		}
 	}
-
-	public boolean early()
+	// Get method for isEarly
+	public boolean getIsEarly()
 	{
 		return isEarly;
 	}
-
+	// Calculate the fee with a bunch of if statements
 	public void calcFee(int enHr, int enMin, int exHr, int exMin, boolean mem)
 	{
+		// If duration is negative, you messed up and deserve to pay the whole fine
 		if (duration < 0)
 		{
 			fee = 15;
 		}
+		// If duration is less than 5 minutes, you probably couldn't find parking and
+		// don't deserve to get fined
 		else if (duration < 5)
 		{
 			fee = 0;
 		}
+		// Otherwise....
 		else
 		{
+			// Calculate parking price here, maximum charge is $15
+			// Rate is $2 for each 30 minute block of time the car is present in the parking deck
+			// I.E. any time from 31-60 minutes is $4 ($2 + $2)
 			if ((0 < (duration % 30)) && ((duration % 30) < 30))
 			{
 				fee = ((duration / 30) + 1) * 2;
@@ -79,27 +87,33 @@ public class Driver
 				fee = (duration / 30) * 2;
 				tempFee = fee;
 			}
-
+			// Checks to see if the driver is a member of the school
 			if (mem == true)
 			{
+				// Checks to see if the discount will result in a negative charge and prevents it
 				if (tempFee < 3)
 				{
 					fee = 0;
 				}
+				// Otherwise off the $3 discount for members of the school
 				else
 				{
 					fee -= 3;
 				}
+				// Checks if the member is eligible for the early rate
 				if (isEarly == true)
 				{
+					// Checks to see if the senior discounted price is better than early price
 					if ((tempFee < fee) & (tempFee <= 10) & (tempFee <= 15))
 					{
 						fee = tempFee;
 					}
+					// School members who are early have the opportunity to pay a maximum of $7
 					else if (tempFee > 10)
 					{
 						fee = 7;	
 					}
+					// Otherwise use the calculated rates
 					else if (tempFee < fee)
 					{
 						fee = tempFee;
@@ -109,6 +123,7 @@ public class Driver
 						fee = fee;
 					}
 				}
+				// USe calculated rates
 				else if (tempFee < fee)
 				{
 					fee = tempFee;
@@ -118,8 +133,10 @@ public class Driver
 					fee = fee;
 				}
 			}
+			// Checks if driver is eligible for the early rate
 			else if (isEarly == true)
 			{
+				// If early, driver never pays over $10
 				if ((tempFee < fee) & (tempFee <= 10))
 				{
 					fee = tempFee;
@@ -133,6 +150,8 @@ public class Driver
 					fee = 10;
 				}
 			}
+			// Otherwise checks to see if fee is over $15
+			// If it would be, make it $15 then
 			else
 			{
 				if (fee > 15)
@@ -142,7 +161,7 @@ public class Driver
 			}
 		}
 	}
-
+	// Get method for fee
 	public int getFee()
 	{
 		return fee;
--- ParkingDeck.java
+++ ParkingDeck.java
@@ -9,29 +9,33 @@ public class ParkingDeck
 {
 	public static void main(String[] args)
 	{
+		// Default object
 		Driver drive = new Driver();
-
+		// Utilize scanner to query driver
 		Scanner input = new Scanner(System.in);
 		// Query driver on day of the week for free weekend parking
 		System.out.println("Enter today's day of the week");
 		System.out.print("(Sunday - 0, Monday - 1, Tuesday - 2, Wednesday - 3, Thursday - 4, Friday - 5, or Saturday - 6) : ");
 		int day = input.nextInt();
+		// Checks if day inputted is a weekend, if so parking is free
 		if (day == 0 || day == 6)
 		{
 			System.out.println("Parking on the Weekends is free.");
 			System.out.println("Thank you for using our Parking Services!");
 		}
-		
+		// Otherwise, checks for other factors
 		else
 		{
+			// Queries driver if they have a parking permit
 			System.out.println("Do you have a permit? T/F ");
 			boolean permit = input.nextBoolean();
-			
+			//  If so then parking is free
 			if (permit == true)
 			{
 				System.out.println("Permit accepted");
 				System.out.println("Thank you for using our Parking Services!");
 			}
+			// Otherwise, checks for other factors
 			else
 			{
 				// Query driver on duration of stay in parking deck
@@ -43,17 +47,21 @@ public class ParkingDeck
 				int exHr = input.nextInt();
 				System.out.println("Enter exit minute from 0 - 59: ");
 				int exMin = input.nextInt();
+				// Queries the driver if they are a part of the school (Faculty of Student)
 				System.out.println("Are you a member of the school? (Faculty or Student) T/F ");
 				boolean member = input.nextBoolean();
-
+				// Utilize data input for new object
 				Driver drive1 = new Driver(enHr, enMin, exHr, exMin, member);
-				
+				// Calculate duration of stay in minutes
 				drive1.calcDuration(enHr, enMin, exHr, exMin);
+				// Check if driver qualifies for the early rate
 				drive1.checkEarly(enHr, enMin, exHr, exMin);
+				// Calculates the fee with given information
 				drive1.calcFee(enHr, enMin, exHr, exMin, member);
-
-				System.out.printf("Your duration was : %d minutese\n", drive1.getDuration());
-				System.out.printf("Early? %b\n", drive1.early());
+				// Print out receipt at the end
+				System.out.println("Parking Deck Receipt");
+				System.out.printf("Your duration was : %d minutes\n", drive1.getDuration());
+				System.out.printf("Arrived at or before 9:00AM? %b\n", drive1.getIsearly());
 				System.out.println("Your fee is $" + drive1.getFee());
 				System.out.println("Thank you for using our Parking Services!");
 			}
